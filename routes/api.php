d15a7b05851bb9ba2534812fe0425c040ba21a12
shimigo <shimigo1965@yahoo.co.jp>
2022-03-28 21:09:20 +0900

    first commit

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestController;
+
+Route::apiResource('/v1/rest', RestController::class);
+Route::get('/hello', function () {
+    return response()->json([
+        'message' => 'Hello'
+    ], 200);
+});
---
+++ tests/Feature/RestControllerTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Rest;
+
+class RestControllerTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_index_rest()
+    {
+        $items = Rest::factory()->create();
+        $response = $this->get('/api/v1/rest');
+        $response->assertStatus(200);
+        $response->assertJsonFragment([
+            'message' => $items->message,
+            'url' => $items->url
+        ]);
+    }
+    public function test_store_rest()
+    {
+        $data = [
+            'message' => 'rest',
+            'url' => 'rest@example.com',
+        ];
+        $response = $this->post('/api/v1/rest', $data);
+        $response->assertStatus(201);
+        $response->assertJsonFragment($data);
+        $this->assertDatabaseHas('rests', $data);
+    }
+    public function test_show_rest()
+    {
+        $item = Rest::factory()->create();
+        $response = $this->get('/api/v1/rest/' . $item->id);
+        $response->assertStatus(200);
+        $response->assertJsonFragment([
+            'message' => $item->message,
+            'url' => $item->url
+        ]);
+    }
+}
